cbfbc7d61c54d368cdaf0d90421e6545a960393c
Thbop <75861321+thbop@users.noreply.github.com>
2025-01-27 21:04:50 -0500

    Much better

--- math_parse_test.py
+++ math_parse_test.py
@@ -1,13 +1,16 @@
 STATE_NONE = 0
 STATE_INTEGER = 1
 STATE_ARITHMETIC = 2
+STATE_SCOPE = 3
 
 TYPE_INTEGER = 0
 TYPE_ARITHMETIC = 1
+TYPE_SCOPE = 2
 
 
 
-numerals = '0123456789ABCDEF'
+
+numerals = '0123456789'
 arithmetics = '+-*/'
 
 def _build_data_tree(expr: str):
@@ -15,10 +18,14 @@ def _build_data_tree(expr: str):
 
     data = []
     current_dat = -1
+    scopes_to_pass = 0
+
+    current_scope = -1
+
 
     # Build data tree
     for i, c in enumerate(expr.upper()):
-        if c in numerals:
+        if state != STATE_SCOPE and c in numerals:
             if state != STATE_INTEGER:
                 current_dat += 1
                 data.append(
@@ -30,7 +37,7 @@ def _build_data_tree(expr: str):
             else:
                 data[current_dat]['value'] += c
             state = STATE_INTEGER
-        elif c in arithmetics:
+        elif state != STATE_SCOPE and c in arithmetics:
             if state == STATE_ARITHMETIC:
                 print('ERROR: Bad arithmetic!')
                 return None
@@ -43,6 +50,32 @@ def _build_data_tree(expr: str):
                 }
             )
             state = STATE_ARITHMETIC
+        
+        elif c == '(':
+            scopes_to_pass += 1
+            if state != STATE_SCOPE:
+                current_dat += 1
+                current_scope = current_dat
+                data.append(
+                    {
+                        'type': TYPE_SCOPE,
+                        'value': i+1 # Scope start
+                    }
+                )
+                state = STATE_SCOPE
+        
+        elif c == ')':
+            scopes_to_pass -= 1
+            if scopes_to_pass == 0:
+                if current_scope == -1:
+                    print('ERROR: Invalid scope!')
+                    return None
+                scope_start = data[current_scope]['value']
+                scope_end = i
+                data[current_scope]['value'] = _build_data_tree(expr[scope_start:scope_end])
+
+                state = STATE_NONE
+
         
     return data
 
@@ -74,16 +107,25 @@ def _evaluate(data: dict):
         elif entry['type'] == TYPE_ARITHMETIC:
             operation = entry['value']
             left = False
+        elif entry['type'] == TYPE_SCOPE:
+            if left:
+                lvalue = _evaluate(entry['value'])
+                left = False
+            else:
+                rvalue = _evaluate(entry['value'])
+                lvalue = _handle_operation(lvalue, rvalue, operation)
+                left = True
     
     return lvalue
 
 
 def evaluate(expr: str):
     data = _build_data_tree(expr)
+    print(data)
     return _evaluate(data)
 
 
 if __name__ == '__main__':
-    expr = '5 + 2 * 10' # Problem
+    expr = '-2 + 5 + (2 * (5+5))'
     result = evaluate(expr)
     print(result)
